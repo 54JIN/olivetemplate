0235230c3abd023d57f6766eae8e435885e6c51d
54JIN <sajinsaju2002@gmail.com>
2024-09-28 23:26:51 -0400

    Made the Header have a backdrop blur

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="fixed top-0 left-0 w-full z-10">
+    <div className="fixed top-0 left-0 w-full z-10 bg-[rgba(255,255,255,0.8)] backdrop-blur-md">
       <div className="flex justify-center items-center h-[70px]">
         <div className="flex justify-between w-[90vw] sm:w-[90vw] md:w-[90vw] lg:w-[90vw] xl:w-[60vw] 2xl:w-[60vw]">
           <h1>Logo</h1>
